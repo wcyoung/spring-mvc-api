221d9ac8c4481873c3179b47fd403d52e36d407d
Chiyoung Won <wcyoung98@gmail.com>
2019-12-11 23:56:53 +0900

    Abstract Base Class Package 이동

--- src/main/java/wcyoung/spring/mvc/api/version/VersionService.java
+++ src/main/java/wcyoung/spring/mvc/api/version/VersionService.java
@@ -8,7 +8,8 @@ import javax.annotation.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import wcyoung.spring.mvc.common.aop.BaseService;
+import wcyoung.spring.mvc.common.base.BaseService;
+import wcyoung.spring.mvc.mapper.springmvc.SpringMvcMapper;
 
 @Service
 public class VersionService extends BaseService {
@@ -16,9 +17,13 @@ public class VersionService extends BaseService {
     @Resource
     private ResourceLoader resourceLoader;
 
+    @Resource
+    private SpringMvcMapper springMvcMapper;
+
     public String getVersionInfo() throws IOException {
         Properties properties = new Properties();
         properties.load(resourceLoader.getResource("/META-INF/MANIFEST.MF").getInputStream());
+        log.info("{}", springMvcMapper.selectNow());
         return properties.getProperty("Implementation-Version");
     }
 
